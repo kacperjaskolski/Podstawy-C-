a55364be0115a5b1c656f9259a23fbab352ad8ff
Kacper Jaskólski <kapi200505@wp.pl>
2025-02-28 13:17:33 +0100

    Update Program.cs

--- zadanie 7/Program.cs
+++ zadanie 7/Program.cs
@@ -8,8 +8,14 @@ namespace zadanie_7
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
+                   int a, b;
+            Console.Write("podaj a=");
+         a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("podaj b=");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}/{1}={2}r{3}", a, b, a / b, a % b);
+            Console.ReadKey(true);
         }
     }
 }
+
