55a02484905c7a855780ab628528cb3cf7009264
Kacper Jaskólski <kapi200505@wp.pl>
2025-03-10 16:22:02 +0100

    Update Program.cs

--- zad 7 warunki/Program.cs
+++ zad 7 warunki/Program.cs
@@ -18,23 +18,23 @@ namespace zad_7_warunki
              else if (a >= 2000) { a -= 2000; Console.WriteLine("MM"); }
            else  if (a >= 1000) { a -= 1000; Console.WriteLine("M");   }
             if (a >= 900) {  a -=900 ; Console.WriteLine("CM"); }
-            else if (a >= 800) { a -= 800; Console.WriteLine("DCCC"); }
-            else if (a >= 700) { a -= 700; Console.WriteLine("DCC"); }
-            else if (a >= 600) { a -= 600; Console.WriteLine("DC"); }
-            else if (a >= 500) { a -= 500; Console.WriteLine("D"); }
-            else if (a >= 400) { a -= 400; Console.WriteLine("CD"); }
-            else if (a >= 300) { a -= 300; Console.WriteLine("CCC"); }
-            else if (a >= 200) { a -= 200; Console.WriteLine("C"); }
-            else if (a >= 100) { a -= 100; Console.WriteLine("C"); }
+            else if (a >= 800) { a -= 800; Console.Write("DCCC"); }
+            else if (a >= 700) { a -= 700; Console.Write("DCC"); }
+            else if (a >= 600) { a -= 600; Console.Write("DC"); }
+            else if (a >= 500) { a -= 500; Console.Write("D"); }
+            else if (a >= 400) { a -= 400; Console.Write("CD"); }
+            else if (a >= 300) { a -= 300; Console.Write("CCC"); }
+            else if (a >= 200) { a -= 200; Console.Write("C"); }
+            else if (a >= 100) { a -= 100; Console.Write("C"); }
             if (a >= 90) { a -= 90; Console.WriteLine("XC"); }
-            else if (a >= 80) { a -= 80; Console.WriteLine("LXXX"); }
-            else if (a >= 70) { a -= 70; Console.WriteLine("LXX"); }
-            else if (a >= 60) { a -= 100; Console.WriteLine("LX"); }
-            else if (a >= 50) { a -= 50; Console.WriteLine("L"); }
-            else if (a >= 40) { a -= 40; Console.WriteLine("XL"); }
-            else if (a >= 30) { a -= 30; Console.WriteLine("XXX"); }
-            else if (a >= 20) { a -= 20; Console.WriteLine("XX"); }
-            else if (a >= 10) { a -= 10; Console.WriteLine("X"); }
+            else if (a >= 80) { a -= 80; Console.Write("LXXX"); }
+            else if (a >= 70) { a -= 70; Console.Write("LXX"); }
+            else if (a >= 60) { a -= 100; Console.Write("LX"); }
+            else if (a >= 50) { a -= 50; Console.Write("L"); }
+            else if (a >= 40) { a -= 40; Console.Write("XL"); }
+            else if (a >= 30) { a -= 30; Console.Write("XXX"); }
+            else if (a >= 20) { a -= 20; Console.Write("XX"); }
+            else if (a >= 10) { a -= 10; Console.Write("X"); }
             switch (a)
             {
                 case 1: Console.WriteLine("I"); break;
