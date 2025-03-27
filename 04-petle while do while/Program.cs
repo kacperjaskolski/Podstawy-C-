3a5ac91f9da6a8fc56b1a042712c5a6d97dcfdc2
kapi2 <kapi2@JASKOL9>
2025-03-27 01:04:58 +0100

    koniec lk

---
+++ 04-petle while do while/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_petle_while_do_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
