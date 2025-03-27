3eb3f0814930fa7e11db20b9c9f2c851f4b847fa
kapi2 <kapi2@JASKOL9>
2025-03-27 01:10:55 +0100

    koniec z

---
+++ zad 2 p/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Dany jest wzór ciągu a(n)=2n-1. Napisz program wczytujący liczbę całkowitą dodatnią n i
+//wypisujący na ekranie kolejne wyrazy ciągu: a(1), a(2), …, a(n).Użyj pętli while.
+namespace zad2_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, an, i;
+            Console.WriteLine("podaj  n:");
+            n = Convert.ToInt32(Console.ReadLine());
+            i = 0;
+            while (++i <= n)
+            {
+                an = 2 * i - 1;
+                Console.WriteLine("a({0}) ={1}", i, an);
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
